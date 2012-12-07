768e0d951a497896b00fbcff0d2ebf5c81c5d48a
fpischedda <francesco.pischedda@gmail.com>
2012-12-07 11:00:56 +0100

    added use function to Item and relative tests

--- src/core/item.py
+++ src/core/item.py
@@ -4,6 +4,10 @@ __date__ = "$13-mar-2012 16.19.40$"
 from utils.equality_comparable import EqualityComparable
 
 
+class ItemNotUsableException(Exception):
+    pass
+
+
 class Item(EqualityComparable):
 
     def __init__(self, name, quantity, usable, user_id):
@@ -12,3 +16,15 @@ class Item(EqualityComparable):
         self.usable = usable
         self.quantity = quantity
         self.user_id = user_id
+
+    def use(self, quantity):
+
+        if not self.usable:
+            raise ItemNotUsableException
+
+        if quantity > self.quantity:
+            quantity = self.quantity
+
+        self.quantity -= quantity
+
+        return quantity
--- src/core/test_item.py
+++ src/core/test_item.py
@@ -1,12 +1,28 @@
+from nose.tools import raises
 from item import Item
+from item import ItemNotUsableException
 
 
-def test_unserialization_serialization():
+def tes_usable_item():
 
-    item = Item('item', 1, True)
+    i = Item('name', 10, True, 0)
+    quantity = 10
 
-    s = item.serialize()
+    assert quantity == i.use(quantity)
 
-    unserialized = Item.unserialize(s)
 
-    assert unserialized == item
+def test_usable_item_small_quantity():
+
+    i = Item('name', 5, True, 0)
+    quantity = 10
+
+    assert quantity > i.use(quantity)
+
+
+@raises(ItemNotUsableException)
+def test_not_usable_item():
+
+    i = Item('name', 10, False, 0)
+    quantity = 10
+
+    i.use(quantity)
