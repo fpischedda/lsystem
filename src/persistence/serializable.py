4cdaa5855f5f2e9329ad3bf0b7297f4a41aab8a6
fpischedda <francesco.pischedda@gmail.com>
2012-12-07 23:12:25 +0100

    finished new class-serializable mapping and tests

--- src/persistence/serializable.py
+++ src/persistence/serializable.py
@@ -3,35 +3,111 @@ from collections import namedtuple
 
 class Serializable(object):
 
+    ID_FIELDS = None
     FIELDS = None
+
+    def get_id(self):
+
+        return {name: self.__dict__[name] for name in self.__class__.ID_FIELDS}
 
     @classmethod
     def unserialize(cls, obj):
-        return cls(**obj)
+
+        print("class %s" % cls)
+        print("object %s" % obj)
+        new_obj = {f.name: f.unserialize_func(obj[f.name]) for f in cls.FIELDS}
+
+        print(new_obj)
+        return cls(**new_obj)
 
     def serialize(self):
 
-        return {f.name: f.func(self, f.name) for f in self.FIELDS}
+        return {f.name: f.serialize_func(self, f.name) for f in self.FIELDS}
 
-    def direct_field(self, name):
+    @classmethod
+    def list_unserialize(cls, obj):
 
-        return self.__dict__[name]
+        return [cls.unserialize(o) for o in obj]
 
-    def serialize_field(self, name):
+    def list_serialize(self, name):
 
-        return self.__dict__[name].serialize()
-
-    def serialize_list(self, name):
-
-        values = self.direct_field(name)
+        values = self.__dict__[name]
 
         return [e.serialize() for e in values]
 
     @classmethod
-    def declare_field(cls, name, func=None):
+    def direct_unserialize(cls, value):
+        return value
 
-        f = func or cls.direct_field
+    def direct_serialize(self, name):
 
-        return SerializableField(name, f)
+        return self.__dict__[name]
 
-SerializableField = namedtuple('SerializableField', ['name', 'func'])
+    @classmethod
+    def nested_unserialize(cls, value):
+
+        return cls.unserialize(value)
+
+    def nested_serialize(self, name):
+
+        return self.__dict__[name].serialize()
+
+    @classmethod
+    def reference_unserialize(cls, reference):
+
+        return cls.unserialize(cls.get_by_reference(reference))
+
+    def reference_serialize(self, name):
+
+        return self.__dict__[name].get_id()
+
+    @classmethod
+    def get_by_reference(cls, reference):
+
+        raise Exception("This serializable is not referentiable")
+
+    @classmethod
+    def reference_list_unserialize(cls, list):
+
+        return [cls.unserialize(cls.get_by_reference(r)) for r in list]
+
+    def reference_list_serialize(self, name):
+
+        return [r.get_id() for r in self.__dict__[name]]
+
+    @classmethod
+    def field(cls, name):
+
+        return SerializableField(name, cls.direct_serialize,
+                                 cls.direct_unserialize, None)
+
+    @classmethod
+    def nested(cls, name, type):
+
+        return SerializableField(name, cls.nested_serialize,
+                                 type.nested_unserialize, type)
+
+    @classmethod
+    def list(cls, name, type):
+
+        return SerializableField(name, cls.list_serialize,
+                                 type.list_unserialize, type)
+
+    @classmethod
+    def reference(cls, name, type):
+
+        return SerializableField(name, cls.reference_serialize,
+                                 type.reference_unserialize, type)
+
+    @classmethod
+    def reference_list(cls, name, type):
+
+        return SerializableField(name, cls.reference_list_serialize,
+                                 type.reference_list_unserialize, type)
+
+
+SerializableField = namedtuple('SerializableField',
+                               ['name',
+                                'serialize_func',
+                                'unserialize_func',
+                                'field_class'])
---
+++ src/persistence/test_serializable.py
@@ -0,0 +1,161 @@
+from persistence.serializable import Serializable
+
+
+class Field(Serializable):
+
+    ID_FIELDS = ('id_field', )
+    FIELDS = (Serializable.field('id_field'), )
+
+    def __init__(self, id_field):
+
+        self.id_field = id_field
+
+
+class ReferencedField(Serializable):
+
+    ID_FIELDS = ('id_field', )
+    FIELDS = (Serializable.field('id_field'),
+              Serializable.field('some_field'))
+
+    def __init__(self, id_field, some_field):
+
+        self.id_field = id_field
+        self.some_field = some_field
+
+    @classmethod
+    def get_by_reference(cls, id):
+
+        return {'id_field': id['id_field'], 'some_field': 'some value'}
+
+
+class Nested(Serializable):
+
+    FIELDS = (Serializable.nested('nested', Field), )
+
+    def __init__(self, nested):
+
+        self.nested = nested
+
+
+class List(Serializable):
+
+    FIELDS = (Serializable.list('a_list', Field), )
+
+    def __init__(self, a_list):
+
+        self.a_list = a_list
+
+
+class Reference(Serializable):
+
+    FIELDS = (Serializable.reference('referenced', ReferencedField), )
+
+    def __init__(self, referenced):
+
+        self.referenced = referenced
+
+
+class ReferenceList(Serializable):
+
+    FIELDS = (Serializable.reference_list('list_of_referenced', ReferencedField), )
+
+    def __init__(self, list_of_referenced):
+
+        self.list_of_referenced = list_of_referenced
+
+
+def test_field_serialization():
+    """test serialization of a class with a simple field"""
+    f = Field('simple')
+
+    s = f.serialize()
+
+    assert s == {'id_field': 'simple'}
+
+
+def test_field_unserialization():
+    """test unserialization of a class with a simple field"""
+
+    s = {'id_field': 'simple'}
+    obj = Field.unserialize(s)
+
+    assert obj.id_field == 'simple'
+
+
+def test_nested_serialization():
+    """test serialization of a class with a nested field"""
+
+    f = Field('nested')
+    s = Nested(f).serialize()
+
+    assert s['nested'] == {'id_field': 'nested'}
+
+
+def test_nested_unserialization():
+    """test unserialization of a class with a nested field"""
+
+    s = {'nested': {'id_field': 'nested'}}
+    obj = Nested.unserialize(s)
+
+    assert obj.nested.id_field == 'nested'
+
+
+def test_list_serialization():
+    """test serialization of a class with a list field"""
+
+    f1 = Field('nested_in_list1')
+    f2 = Field('nested_in_list2')
+    s = List([f1, f2]).serialize()
+
+    assert s['a_list'] == [{'id_field': 'nested_in_list1'},
+                           {'id_field': 'nested_in_list2'}]
+
+
+def test_list_unserialization():
+    """test unserialization of a class with a list field"""
+
+    s = {'a_list': [{'id_field': 'nested_in_list1'},
+                    {'id_field': 'nested_in_list2'}]}
+
+    obj = List.unserialize(s)
+
+    assert obj.a_list[0].id_field == 'nested_in_list1' and obj.a_list[1].id_field == 'nested_in_list2'
+
+
+def test_reference_serialization():
+    """test serialization of a class with a reference field"""
+
+    f = ReferencedField('some id', 'some value')
+    s = Reference(f).serialize()
+
+    print(s)
+    assert s == {'referenced': {'id_field': 'some id'}}
+
+
+def test_reference_unserialization():
+    """test unserialization of a class with a reference field"""
+    s = {'referenced': {'id_field': 'some id'}}
+    r = Reference.unserialize(s)
+
+    assert r.referenced.some_field == 'some value'
+
+
+def test_reference_list_serialization():
+    """test serialization of a class with a list of reference fields"""
+
+    r1 = ReferencedField('referenced_in_list1', 'value1')
+    r2 = ReferencedField('referenced_in_list2', 'value2')
+    s = ReferenceList([r1, r2]).serialize()
+
+    assert s['list_of_referenced'] == [{'id_field': 'referenced_in_list1'},
+                                       {'id_field': 'referenced_in_list2'}]
+
+
+def test_reference_list_unserialization():
+    """test unserialization of a class with a list of referenced fields"""
+
+    s = {'list_of_referenced': [{'id_field': 'referenced_in_list1'},
+                                {'id_field': 'referenced_in_list2'}]}
+
+    r = ReferenceList.unserialize(s)
+    assert r.list_of_referenced[0].some_field == 'some value'
